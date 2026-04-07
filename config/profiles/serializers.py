eba32639b1ed31b7b6d4861dc53450731000376f
engenamyocy <engenamyocy@gmail.com>
2026-04-07 16:32:39 +0500

    edit models, serializers

--- config/profiles/serializers.py
+++ config/profiles/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-from .models import Profile
-
+from .models import Profile, Gender, FirstName, LastName, Bio, Age, Avatar, City, Country
 
 class ProfileSerializer(serializers.ModelSerializer):
 
@@ -17,8 +16,52 @@ class ProfileSerializer(serializers.ModelSerializer):
             "age",
             "gender",
             "city",
-            "country",
-            "avatar",
-            "created_at",
-            "updated_at",
+            "country"
         )
+
+class FirstNameSerializers (serializers.ModelSerializer):
+    class Meta:
+        model = FirstName
+        fields = '__all__'
+
+class LastNameSerializers (serializers.ModelSerializer):
+    class Meta:
+        model = LastName
+        fields = '__all__'
+    
+class BioSerializers (serializers.ModelSerializer):
+    class Meta:
+        model = Bio
+        fields = '__all__'
+    
+class AgeSerializers (serializers.ModelSerializer):
+    class Meta:
+        model = Age
+        fields = '__all__'
+    
+class GenderSerializers (serializers.ModelSerializer):
+    class Meta:
+        model = Gender
+        fields = '__all__'
+    
+class CitySerializers (serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = '__all__'
+    
+class CountrySerializers (serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = '__all__'
+    
+class AvatarSerializers (serializers.ModelSerializer):
+    class Meta:
+        model = Avatar
+        fields = '__all__'
+
+class ProfilesSerializers(serializers.ModelSerializer):
+    first_name = serializers.CharField(source = 'first.name', read_only = True)
+    last_name = serializers.CharField(source = 'last.name', read_only = True)
+    class Meta:
+        model = Profile
+        fields = '__all__'
