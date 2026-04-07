a1da3300436b2e404ade50d3da0b6a22aafa8dfa
engenamyocy <engenamyocy@gmail.com>
2026-04-07 16:32:03 +0500

    Delete profiles directory

--- profiles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ProfileListView, ProfileCreateView, ProfileDetailView, ProfileMeView
-
-urlpatterns = [
-    path("profiles/", ProfileListView),
-    path("profiles/create/", ProfileCreateView),
-    path("profiles/<int:pk>/", ProfileDetailView),
-]
