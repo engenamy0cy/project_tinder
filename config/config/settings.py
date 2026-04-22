84bb190adf67bca4179ec2b6895a2f9a7d216c11
engenamyocy <engenamyocy@gmail.com>
2026-04-22 12:00:56 +0500

    tech

--- config/config/settings.py
+++ config/config/settings.py
@@ -43,8 +43,9 @@ INSTALLED_APPS = [
     'users',
     'profiles',
     'rest_framework',
-    'rest_framework.authtoken',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -129,10 +130,8 @@ AUTH_USER_MODEL = "users.User"
 
 # DRF settings: mobile client will authenticate via token.
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.TokenAuthentication",
-    ),
-    "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticated",
-    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
 }
+
