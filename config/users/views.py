85402a69fffd8981fbc11d5066719311ce2969b6
engenamyocy <engenamyocy@gmail.com>
2026-06-02 15:15:01 +0500

    dodelat' doma

--- config/users/views.py
+++ config/users/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
 
@@ -7,3 +8,24 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        if not serializer.is_valid():
+            print(serializer.errors)
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        return super().create(request, *args, **kwargs)
+
+@api_view(["POST"])
+def register(request):
+    return Response({"ok": True})
+
+
+@api_view(["POST"])
+def login(request):
+    return Response({"ok": True})
